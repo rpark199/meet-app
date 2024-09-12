078773bb5ba846d79eb1e5cd2cd512f64492057f
rpark199 <147426615+rpark199@users.noreply.github.com>
2024-09-12 16:49:43 -0400

    Added test for show detail button (Line 45-69)

--- src/_tests_/Event.test.js
+++ src/_tests_/Event.test.js
@@ -42,4 +42,29 @@ describe("<Event /> component", () => {
     expect(EventComponent.queryByText("Show Details")).toBeInTheDocument();
   });
 
+  test("by default, event's details section should be hidden", () => {
+    const eventDetails = EventComponent.container.querySelector('.details');
+    expect (eventDetails).not.toBeInTheDocument;
+  });
+
+  test("show details after user clicks on button 'Show Details'", async () => {
+    const user = userEvent.setup();
+    const button = EventComponent.queryByText('Show Details');
+    await user.click(button);
+    const details = EventComponent.container.querySelector('.details');
+    expect (details).toBeInTheDocument;
+  });
+
+  test("hide details after use clicks on button 'Hide details'", async () => {
+    const user = userEvent.setup();
+    let button = EventComponent.queryByText('Show Details');
+    await user.click(button);
+    let details = EventComponent.container.querySelector('.details');
+    expect (details).toBeInTheDocument;
+
+    button = EventComponent.queryByText('Hide Details');
+    await user.click(button);
+    details = EventComponent.container.querySelector('.details');
+    expect (details).not.toBeInTheDocument;
+  });
 });
