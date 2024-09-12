c6b28bed831d76ff4d1b839e8946ea811a6ff337
rpark199 <147426615+rpark199@users.noreply.github.com>
2024-09-12 10:09:25 -0400

    edited describe component

--- src/components/EventList.js
+++ src/components/EventList.js
@@ -1,12 +1,19 @@
-import Event from './Event';
+import { render } from '@testing-library/react';
+import EventList from '../components/EventList';
+import { getEvents } from '../api';
 
-const EventList = ({ events }) => {
-  return (
-    <ul id="event-list">
-      {events? 
-        events.map(event => <Event key={event.id} event={event} />) : null}
-    </ul>
-  );
-}
-
-export default EventList;
+describe('<EventList /> component', () => {
+    let EventListComponent;
+    beforeEach(() => {
+        EventListComponent = render(<EventList />);
+    })
+    test('has an element with "list" role', () => {
+        expect(EventListComponent.queryByRole("list")).toBeInTheDocument();
+    });
+    
+    test('renders correct number of events', async () => {
+        const allEvents = await getEvents(); 
+        EventListComponent.rerender(<EventList events={allEvents} />);
+        expect(EventListComponent.getAllByRole("listitem")).toHaveLength(allEvents.length);
+    });
+});
