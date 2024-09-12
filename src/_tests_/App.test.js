0368a66e074b315661d376b8ee59577d15b8a342
rpark199 <147426615+rpark199@users.noreply.github.com>
2024-09-12 10:16:08 -0400

    Edited describe component

--- src/_tests_/App.test.js
+++ src/_tests_/App.test.js
@@ -5,13 +5,17 @@ describe('<App /> component', () => {
   let AppDOM;
   beforeEach(() => {
     AppDOM = render(<App />).container.firstChild;
-  })
-
+  });
+    
   test('renders list of events', () => {
     expect(AppDOM.querySelector('#event-list')).toBeInTheDocument();
   });
-
+    
   test('render CitySearch', () => {
     expect(AppDOM.querySelector('#city-search')).toBeInTheDocument();
+  });
+   
+  test('render NumberOfEvents', () => {
+    expect(AppDOM.querySelector('#number-of-events')).toBeInTheDocument();
   });
 });
