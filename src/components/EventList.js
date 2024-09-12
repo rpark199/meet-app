d1040903f258b3162c93ab444ef0bb04940ac37e
rpark199 <147426615+rpark199@users.noreply.github.com>
2024-09-12 10:11:05 -0400

    Update EventList.js

--- src/components/EventList.js
+++ src/components/EventList.js
@@ -1,19 +1,12 @@
-import { render } from '@testing-library/react';
-import EventList from '../components/EventList';
-import { getEvents } from '../api';
+import Event from './Event';
 
-describe('<EventList /> component', () => {
-    let EventListComponent;
-    beforeEach(() => {
-        EventListComponent = render(<EventList />);
-    })
-    test('has an element with "list" role', () => {
-        expect(EventListComponent.queryByRole("list")).toBeInTheDocument();
-    });
-    
-    test('renders correct number of events', async () => {
-        const allEvents = await getEvents(); 
-        EventListComponent.rerender(<EventList events={allEvents} />);
-        expect(EventListComponent.getAllByRole("listitem")).toHaveLength(allEvents.length);
-    });
-});
+const EventList = ({ events }) => {
+  return (
+    <ul id="event-list">
+      {events? 
+        events.map(event => <Event key={event.id} event={event} />) : null}
+    </ul>
+  );
+}
+
+export default EventList;
