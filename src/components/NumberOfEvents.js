a3bb5563c6a530c5d0db6b83526ebf475a2cb562
John Akhilomen <jonbasanti@gmail.com>
2024-09-16 18:35:48 -0500

    Updates

--- src/components/NumberOfEvents.js
+++ src/components/NumberOfEvents.js
@@ -1,15 +1,6 @@
-const NumberOfEvents = ({ setCurrentNOE, setErrorAlert }) => {
+const NumberOfEvents = ({ setCurrentNOE }) => {
   const handleInputChanged = (event) => {
       const value = event.target.value;
-
-      if(isNaN(value) || value <= 0) {
-          setErrorAlert('Enter a valid number');
-      } else if (value > 32) {
-          setErrorAlert('Only maximum of 32 is allowed');
-      } else {
-          setErrorAlert('');
-          setCurrentNOE(value);
-      }
   };
 
   return (
