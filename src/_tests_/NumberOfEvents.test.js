814e3c676256e08be45d8ac9a8c852fac10e870e
rpark199 <147426615+rpark199@users.noreply.github.com>
2024-09-17 19:26:56 -0400

    Update NumberOfEvents.test.js

--- src/_tests_/NumberOfEvents.test.js
+++ src/_tests_/NumberOfEvents.test.js
@@ -14,20 +14,22 @@ describe('<NumberOfEvents /> Component', () => {
         );
     });
 
-    test('component contains input textbox', () => {
+    test('render element with role of textbox', () => { 
         const input = NumberOfEventsComponent.queryByRole('textbox');
-        expect(input).toBeInTheDocument();
-    });
+        expect(input).toBeInTheDocument(); 
+      });
     
     test('ensures the default value of textbox is 32', () => {
         const input = NumberOfEventsComponent.queryByRole('textbox');
         expect(input).toHaveValue('32');
     });
 
-    test('textbox value changes when user updates input', async () => {
-        const input = NumberOfEventsComponent.getByTestId('numberOfEventsInput');
-        const user = userEvent.setup();
-        await user.type(input, '{backspace}{backspace}10');
-        expect(input).toHaveValue('10');
-    })
-})
+    test('change number of events when a user types in the textbox', async () => { 
+        const numverOfEvents = NumberOfEventsComponent.getByRole('textbox');
+        const user = userEvent.setup(); 
+        await user.type(numverOfEvents, '{backspace}{backspace}10');   
+        const allEvents = await getEvents(); 
+        NumberOfEventsComponent.rerender(<NumberOfEvents setCurrentNOE={allEvents} setErrorAlert={() => {}} />);   
+        expect(numverOfEvents).toHaveValue('10'); 
+    }); 
+});
