fb48eb394bf876961e267e6fc2b000f5d2f2a1fa
John Akhilomen <jonbasanti@gmail.com>
2024-09-23 16:12:15 -0500

    updated package.json

--- src/api.js
+++ src/api.js
@@ -1,18 +1,13 @@
+
 import mockData from './mock-data';
 
-/**
- *
- * @param {*} events:
- * The following function should be in the “api.js” file.
- * This function takes an events array, then uses map to create a new array with only locations.
- * It will also remove all duplicates by creating another new array using the spread operator and spreading a Set.
- * The Set will remove all duplicates from the array.
- */
+
 export const extractLocations = (events) => {
   const extractedLocations = events.map((event) => event.location);
-  const locations = [...new Set(extractedLocations)];
+  const locations = [...new Set(extractedLocations)];   // Spread operator expands an iterable inside a specified receiver.
   return locations;
 };
+
 
 const checkToken = async (accessToken) => {
   const response = await fetch(
@@ -20,56 +15,6 @@ const checkToken = async (accessToken) => {
   );
   const result = await response.json();
   return result;
-};
-
-export const getAccessToken = async () => {
-  const accessToken = localStorage.getItem('access_token');
-  const tokenCheck = accessToken && (await checkToken(accessToken));
-
-  if (!accessToken || tokenCheck.error) {
-    await localStorage.removeItem("access_token");
-    const searchParams = new URLSearchParams(window.location.search);
-    const code = await searchParams.get("code");
-    if (!code) {
-      const response = await fetch(
-        "https://v9f320if2i.execute-api.eu-central-1.amazonaws.com/dev/api/token"
-      );
-      const result = await response.json();
-      const { authUrl } = result;
-      return (window.location.href = authUrl);
-    }
-    return code && getToken(code);
-  }
-  return accessToken;
-};
-
-const getToken = async (code) => {
-  const encodeCode = encodeURIComponent(code);
-  const response = await fetch(
-    'Yhttps://v9f320if2i.execute-api.eu-central-1.amazonaws.com/dev/api/token' + '/' + encodeCode
-  );
-  const { access_token } = await response.json();
-  access_token && localStorage.setItem("access_token", access_token);
-
-  return access_token;
-};
-
-export const getEvents = async () => {
-  if (window.location.href.startsWith('http://localhost')) {
-    return mockData;
-  }
-
-  const token = await getAccessToken();
-
-  if (token) {
-    removeQuery();
-    const url =  "https://v9f320if2i.execute-api.eu-central-1.amazonaws.com/dev/api/get-events" + "/" + token;
-    const response = await fetch(url);
-    const result = await response.json();
-    if (result) {
-      return result.events;
-    } else return null; 
-  }
 };
 
 const removeQuery = () => {
@@ -85,4 +30,72 @@ const removeQuery = () => {
     newurl = window.location.protocol + "//" + window.location.host;
     window.history.pushState("", "", newurl);
   }
+};
+
+const getToken = async (code) => {
+  try {
+    const encodeCode = encodeURIComponent(code);
+    const response = await fetch(
+      
+      'https://v9f320if2i.execute-api.eu-central-1.amazonaws.com/dev/api/token' + '/' + encodeCode
+    );
+    const { access_token } = await response.json();
+    access_token && localStorage.setItem("access_token", access_token);
+
+    return access_token;
+
+  } catch (error) {
+    error.json();
+  }
+};
+
+
+export const getEvents = async () => {
+  // return mockData;
+  if (window.location.href.startsWith('http://localhost')) {
+    return mockData;
+  }
+  
+  if (!navigator.onLine) {
+    const events = localStorage.getItem("lastEvents");
+    // NProgress.done();
+    return events?JSON.parse(events):[];
+  }
+
+  const token = await getAccessToken();
+  console.log(token);
+  if (token) {
+    removeQuery();
+    const url = "https://v9f320if2i.execute-api.eu-central-1.amazonaws.com/dev/api/get-events" + "/" + token;
+    const response = await fetch(url);
+    const result = await response.json();
+    if (result) {
+      localStorage.setItem("lastEvents", JSON.stringify(result.events));
+      return result.events;
+    } else return null;
+  }
+};
+
+
+export const getAccessToken = async () => {
+  const accessToken = localStorage.getItem('access_token');
+
+  const tokenCheck = accessToken && (await checkToken(accessToken));
+
+  if (!accessToken || tokenCheck.error) {
+    await localStorage.removeItem("access_token");
+    const searchParams = new URLSearchParams(window.location.search);
+    const code = await searchParams.get("code");
+    if (!code) {
+      const response = await fetch(
+        "https://v9f320if2i.execute-api.eu-central-1.amazonaws.com/dev/api/get-auth-url"
+      );
+      const result = await response.json();
+      const { authUrl } = result;
+      return (window.location.href = authUrl);
+    }
+    return code && getToken(code);
+  }
+  return accessToken;
+
 };
