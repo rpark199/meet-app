e448cd60963cef023aca74b40abf4d2d03023c4d
rpark199 <147426615+rpark199@users.noreply.github.com>
2024-09-23 17:48:41 -0400

    Fixed line 6 from meet to meet-app

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -3,7 +3,7 @@ const calendar = google.calendar("v3");
 const SCOPES = ["https://www.googleapis.com/auth/calendar.events.public.readonly"];
 const { CLIENT_SECRET, CLIENT_ID, CALENDAR_ID } = process.env;
 const redirect_uris = [
-  "https://rpark199.github.io/meet/"
+  "https://rpark199.github.io/meet-app"
 ];
 
 const oAuth2Client = new google.auth.OAuth2(
