6140bfa288190a459959598fe1b9b375455ec97e
rpark199 <147426615+rpark199@users.noreply.github.com>
2024-10-08 16:55:22 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,16 +4,19 @@ import EventList from "./components/EventList";
 import NumberOfEvents from "./components/NumberOfEvents";
 import { getEvents, extractLocations } from "./api";
 import "./App.css";
-import mockData from "./mock-data";
-
+import { mockData } from "./mock-data";
+import { InfoAlert, ErrorAlert, WarningAlert } from './components/Alert';
 
 const App = () => {
   const [allLocations, setAllLocations] = useState([]);
   const [currentNOE, setCurrentNOE] = useState(32);
   const [events, setEvents] = useState([]);
   const [currentCity, setCurrentCity] = useState("See all cities");
+  const [infoAlert, setInfoAlert] = useState("");
+  const [errorAlert, setErrorAlert] = useState("");
+  const [warningAlert, setWarningAlert] = useState("");
 
- const fetchData = async () => {
+  const fetchData = async () => {
     const allEvents = await getEvents();
     const filteredEvents = currentCity === "See all cities" ? 
       allEvents :
@@ -24,12 +27,28 @@ const App = () => {
 
   useEffect(() => {
     fetchData();
-  }, [currentCity]);
+  }, [currentCity, currentNOE]);
 
   return (
     <div className="App">
-      <CitySearch allLocations={allLocations} setCurrentCity={setCurrentCity} />
-      <NumberOfEvents currentNOE = {currentNOE} setCurrentNOE={setCurrentNOE} />
+      <section className="header">
+      <h1>Meet App</h1>
+      <p>Find events in nearby cities</p>
+      <div className = "alerts-container">
+        {infoAlert.length ? <InfoAlert text={infoAlert}/> : null}
+        {errorAlert.length ? <ErrorAlert text={errorAlert} /> : null}
+        {warningAlert.length ? <WarningAlert text={warningAlert} /> : null}
+      </div>
+      <CitySearch 
+        allLocations={allLocations} 
+        setCurrentCity={setCurrentCity}
+        setInfoAlert={setInfoAlert} 
+      />
+      <NumberOfEvents 
+        currentNOE = {currentNOE} 
+        setCurrentNOE={setCurrentNOE} 
+      />
+      </section>
       <EventList events={events} />
     </div>
   );
