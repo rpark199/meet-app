e9c1de3b53814ef9394e71977067cbcfd1065134
rpark199 <147426615+rpark199@users.noreply.github.com>
2024-10-12 20:02:26 -0400

    import NProgress and add NProgress

--- src/api.js
+++ src/api.js
@@ -1,5 +1,5 @@
 import mockData from './mock-data';
-// import NProgress from "nprogess";
+import NProgress from "nprogess";
 
 export const extractLocations = (events) => {
   const extractedLocations = events.map((event) => event.location);
@@ -50,10 +50,16 @@ const getToken = async (code) => {
 };
 
 export const getEvents = async () => {
-  // NProgress.start();
+  NProgress.start();
   if (window.location.href.startsWith('http://localhost')) {
-    // NProgress.done();
+    NProgress.done();
     return mockData;
+  }
+
+  if (!navigator.onLine) {
+    const events = localStorage.getItem("lastEvents");
+    NProgress.done();
+    return events?JSON.parse(events):[];
   }
 
   const token = await getAccessToken();
@@ -66,6 +72,8 @@ export const getEvents = async () => {
     const response = await fetch(url);
     const result = await response.json();
     if (result) {
+      NProgress.done();
+      localStorage.setItem("lastEvents", JSON.stringify(result.events));
       return result.events;
     } else return null; 
   }
