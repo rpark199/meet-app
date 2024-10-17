9afa2eb093e82738e4d25dfd8792ff0047240a66
rpark199 <147426615+rpark199@users.noreply.github.com>
2024-10-16 20:12:36 -0400

    Deleted extra import

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
